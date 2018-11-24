487c9dab62eddf047ed0b9e34091eea863e9bf63
Roxk-33 <xiaorock@126.com>
2018-11-24 21:05:46 +0800

    feat:添加路由懒加载

--- src/common/router.js
+++ src/common/router.js
@@ -1,23 +1,32 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
 import Layout from '@/views/layout';
 import userLogin from '@/views/page/user-login';
-import userRegister from '@/views/page/user-register';
 import user from '@/views/page/user';
-import userInfo from '@/views/page/user-info';
-import userAddressList from '@/views/page/user-address-list';
-import userAddressInfo from '@/views/page/user-address-info';
 import shopList from '@/views/page/shop-list';
-import shopDetail from '@/views/page/shop-detail';
-import orderCreate from '@/views/page/order-create';
-import orderPay from '@/views/page/order-pay';
 import userOrderList from '@/views/page/user-order-list';
-import userCartList from '@/views/page/user-cart-list';
-import userOrderDetail from '@/views/page/user-order-detail';
-import userOrderEvaluation from '@/views/page/user-order-evaluation';
-import addressMap from '@/views/page/address-map';
-import error from '@/views/page/error';
 
+const shopDetail = () => import(/* webpackChunkName: "lazy-detail" */ '@/views/page/shop-list');
+
+const userOrderEvaluation = () =>
+  import(/* webpackChunkName: "lazy-router" */ '@/views/page/user-order-evaluation');
+const userAddressInfo = () =>
+  import(/* webpackChunkName: "lazy-router" */ '@/views/page/user-address-info');
+const userAddressList = () =>
+  import(/* webpackChunkName: "lazy-router" */ '@/views/page/user-address-list');
+const userOrderDetail = () =>
+  import(/* webpackChunkName: "lazy-router" */ '@/views/page/user-order-detail');
+const userCartList = () =>
+  import(/* webpackChunkName: "lazy-router" */ '@/views/page/user-cart-list');
+const userRegister = () =>
+  import(/* webpackChunkName: "lazy-router" */ '@/views/page/user-register');
+
+const orderPay = () => import(/* webpackChunkName: "lazy-router" */ '@/views/page/order-pay');
+const orderCreate = () => import(/* webpackChunkName: "lazy-router" */ '@/views/page/order-create');
+const userInfo = () => import(/* webpackChunkName: "lazy-router" */ '@/views/page/user-info');
+const error = () => import(/* webpackChunkName: "lazy-router" */ '@/views/page/error');
+const addressMap = () => import(/* webpackChunkName: "lazy-router" */ '@/views/page/address-map');
 Vue.use(Router);
 
 export const constantRouterMap = [
