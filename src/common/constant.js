ec3b3b70965e8d130e95d11e7efebf43a916ad91
xiaorock <xiaozhihao@zuzuche.com>
2019-03-09 23:00:57 +0800

    type:调整文案

--- src/common/constant.js
+++ src/common/constant.js
@@ -37,24 +37,26 @@ export default {
     UNPAY: '等待支付',
     PAY: '已支付',
     ACCEPT: '商家已接单',
-    ONTHEWAY: '送达中',
+    ONTHEWAY: '订单配送中',
     ORDER_SUCCESS: '订单已完成',
     ORDER_REFUNDING: '申请退款中',
     ORDER_REFUND: '订单已退款',
     ORDER_CANCEL: '订单已取消',
     ORDER_CANCEL_TIMEOUT: '订单已取消',
+    ORDER_REFUND_FAIL_ONTHEWAY: '订单拒绝退款',
   },
   // 订单状态对应的消息
   ORDER_STATUS_MSG: {
     UNPAY: '请在15分钟内完成支付，超时将自动取消',
     PAY: '订单已支付，等待商家接单',
     ACCEPT: '商家已接单',
-    ONTHEWAY: '送达中',
+    ONTHEWAY: '订单配送中',
     ORDER_SUCCESS: '感谢您对嘻嘻外卖的信任，期待再次光临',
     ORDER_REFUNDING: '订单申请退款中,需要商家确认,请耐心等待',
     ORDER_REFUND: '订单已退款',
     ORDER_CANCEL: '您的订单已取消',
     ORDER_CANCEL_TIMEOUT: '支付超时，订单已取消',
+    ORDER_REFUND_FAIL_ONTHEWAY: '商家拒绝退款,商品配送中',
   },
   // 订单状态进度条
   ORDER_DETAIL_STATUS_TIME: {
@@ -66,5 +68,6 @@ export default {
     cancel_time: '订单已取消',
     apply_refund_time: '申请退款',
     refund_time: '商家同意退款',
+    refund_fail_time: '商家拒绝退款',
   },
 };
