bd786e4d4960c948d185366bb8afa0fc01fef7f5
xiaorock <xiaozhihao@zuzuche.com>
2019-03-24 13:12:59 +0800

    样式优化

--- src/views/page/user-cart-list.vue
+++ src/views/page/user-cart-list.vue
@@ -216,15 +216,14 @@ export default {
 .cart-list {
   overflow: hidden;
   min-height: 100%;
-  background-color: whitesmoke;
 }
 .cart-list-item {
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
   background-color: #fff;
   padding: 0 0 5px;
-  margin: 0.2rem 0.3rem;
+  margin: 0.2rem 0.3rem 0.3rem;
   border-radius: 10px;
-
+  overflow: hidden;
   .shop-info {
     color: #000;
     margin-bottom: 0.2rem;
